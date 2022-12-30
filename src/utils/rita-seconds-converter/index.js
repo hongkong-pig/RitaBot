b9ccf9bf64fa7c17355fc02a1bf2e4c05260f853
shan-ohrae <terry@ohrae.biz>
2022-12-31 07:31:30 +0900

    Apply(feature) : customize settings

--- src/utils/rita-seconds-converter/index.js
+++ src/utils/rita-seconds-converter/index.js
@@ -1,24 +1,28 @@
+// eslint-disable-next-line func-names
 module.exports = function (number, unit) {
-  let d, h, m, s
+  let d = 0;
+  let h = 0;
+  let m = 0;
+  let s = 0;
 
   if (isNaN(number)) {
     throw new TypeError('Value sent to seconds-converter must be a number.')
   }
 
   if (unit === 'sec' || unit === 'seconds') {
-    s = number
+    s = number;
   } else if (unit === 'ms' || unit === 'milliseconds' || !unit) {
-    s = Math.floor(number / 1000)
+    s = Math.floor(number / 1000);
   } else {
-    throw new TypeError('Unit must be sec or ms')
+    throw new TypeError('Unit must be sec or ms');
   }
 
-  m = Math.floor(s / 60)
-  s = s % 60
-  h = Math.floor(m / 60)
-  m = m % 60
-  d = Math.floor(h / 24)
-  h = h % 24
+  m = Math.floor(s / 60);
+  s %= 60;
+  h = Math.floor(m / 60);
+  m %= 60;
+  d = Math.floor(h / 24);
+  h %= 24;
 
-  return {days: d, hours: h, minutes: m, seconds: s}
+  return {days: d, hours: h, minutes: m, seconds: s};
 }
